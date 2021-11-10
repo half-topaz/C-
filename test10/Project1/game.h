cf6bd3747a38acc4c2d4f1f35f54498d10d68fdb
half-topaz <872773228@qq.com>
2021-11-10 19:12:31 +0800

    三子棋

---
+++ test10/Project1/game.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define ROW 3
+#define COL 3
+
+void game(char board[ROW][COL], int row, int col);
+void freshboard(char board[ROW][COL], int row, int col);//初始化棋盘
+void Outputboard(char board[ROW][COL], int row, int col);//打印棋盘
+void humango(char board[ROW][COL], int row, int col);//玩家行动
+void comgo(char board[ROW][COL], int row, int col);//电脑行动
+char checkboard(char board[ROW][COL], int row, int col);//检查棋盘
+int fullboard(char board[ROW][COL], int row, int col);//棋盘下满
---
+++ test10/Project1/test.c
@@ -0,0 +1,34 @@
+#include "game.h"
+
+void menu()
+{
+	printf("***********************************\n");
+	printf("********    1.开始游戏     ********\n");
+	printf("********    0.结束游戏     ********\n");
+	printf("***********************************\n");
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int input = 0;
+	char board[ROW][COL] = { 0 };
+	do 
+	{
+		menu();
+		printf("请选择->");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game(board, ROW, COL);
+			break;
+		case 0:
+			printf("游戏结束\n");
+			break;
+		default:
+			printf("输入异常，请重试\n");
+		}
+	} while (input);
+	return 0;
+}
