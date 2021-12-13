7dc9df37e5f6b4404dab2e5b52c0485feaa85e1f
half-topaz <872773228@qq.com>
2021-12-13 15:03:11 +0800

    1-14

---
+++ try2/Project1/test.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//    printf("\n%d", printf("Hello world!"));
+//    return 0;
+//}
+
+//#include<stdio.h>
+//struct subject {
+//    int No;
+//    float a;
+//    float b;
+//    float c;
+//}score;
+//int main()
+//{
+//    scanf("%d;%f,%f,%f", &score.No, &score.a, &score.b, &score.c);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", score.No, score.a, score.b, score.c);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    char arr[] = { 73,32,99,97,110,32,100,111,32,105,116,33 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    int i = 0;
+//    for (i = 0;i < n;i++)
+//        printf("%c", arr[i]);
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//    int a;
+//    int b;
+//    int c;
+//    scanf("%4d%2d%2d", &a, &b, &c);
+//    printf("year=%04d\nmonth=%02d\ndate=%02d\n", a, b, c);
+//    return 0;
+//}
+
+
